0aa4762deaf8391728efc1a19f0b1a39e5dfa050
Truongnqph23748 <truong290803@gmail.com>
2022-11-13 14:46:32 +0700

    truong commit

--- controllers/client.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-// tạm thời để hiển thị luôn trang admin 
-// sau khi làm đăng nhập thì là admin thì ra trang danh_sach còn là client thì ra trang chủ.
-    function hienThiTrangChu() {
-        
-        require('views/template/home.php');
-    }
-?>
